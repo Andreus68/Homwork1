67b7bb1b78504e39537975a523b403dd454175d7
unknown <perecpon@yandex.ru>
2022-09-17 18:41:34 +0300

    max

---
+++ example2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Example 4
+
+
+Console.WriteLine("Введите numberA: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите numberB: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите numberC: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberA > max) max = numberA; 
+if (numberB > max) max = numberB; 
+if (numberC > max) max = numberC; 
+Console.WriteLine($"max {max}");
+
