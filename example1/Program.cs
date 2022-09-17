ff954b010bcde8cf02afb0e5da8a4ee22a72fa75
unknown <perecpon@yandex.ru>
2022-09-17 18:34:06 +0300

    max,min

---
+++ example1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2
+
+Console.WriteLine("Введите numberA: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите numberB: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+    {
+    Console.WriteLine($"max {numberA} min {numberB}");   
+    }
+else {
+    Console.WriteLine($"max {numberB} min {numberA}");   
+}
+
