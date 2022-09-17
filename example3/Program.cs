04904a681832858450d7b5731af7609e3cff6200
unknown <perecpon@yandex.ru>
2022-09-17 18:48:25 +0300

    even

---
+++ example3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Exampel 6
+
+
+Console.WriteLine("Введите number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 1)
+    {
+    Console.WriteLine($"{number} не четное");   
+    }
+else {
+    Console.WriteLine($"{number} четное");   
+}
